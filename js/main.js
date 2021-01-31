c3827a8a5e7fa68efcc08286edaa2191f7674992
KateLobch <lobchevskaya.kate@gmail.com>
2021-01-31 23:50:55 +0300

    Исправила название переменной

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ const getRandomNumber = (a = 0, b = 0, lengthRemainder = 0) => {
   if (min < 0) {
     return NaN;
   }
-  const number = (Math.floor(Math.random() * (max - min + 1)) + min) / remainder;
-  return number;
+  return (Math.floor(Math.random() * (max - min + 1)) + min) / remainder;
 };
 
-const RandomNumber = getRandomNumber(10, 3, 2);
-alert(RandomNumber);
+const randomNumber = getRandomNumber(10, 3, 2);
+alert(randomNumber);
