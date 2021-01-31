0fb2d5ddb411d2cac3b24f48136d7bd071c09303
KateLobch <lobchevskaya.kate@gmail.com>
2021-01-31 23:42:31 +0300

    Откорретировала функцию

--- js/main.js
+++ js/main.js
@@ -1,30 +1,13 @@
-//Первая часть задания (общая)
-const getRandomNum = function (min, max) {
-  if (min >= 0 && min < max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    let number = (Math.floor(Math.random() * (max - min + 1)) + min);
-    return number;
-  } else {
-    return ('Некорректные значения. Пожалуйста, поменяйте.');
+const getRandomNumber = (a = 0, b = 0, lengthRemainder = 0) => {
+  const remainder = Math.pow(10, lengthRemainder);
+  const min = Math.min(a, b) * remainder;
+  const max = Math.max(a, b) * remainder;
+  if (min < 0) {
+    return NaN;
   }
+  const number = (Math.floor(Math.random() * (max - min + 1)) + min) / remainder;
+  return number;
 };
 
-let RandomNum = getRandomNum(1, 3);
-alert(RandomNum);
-
-// Вторая часть задания (относящаяся к проекту)
-const getRandomNumber = function (min, max, lengthRemainder) {
-  if (min >= 0 && min < max) {
-    let remainder = Math.pow(10, lengthRemainder);
-    min = Math.floor(min * remainder);
-    max = Math.floor(max * remainder);
-    let number = (Math.floor(Math.random() * (max - min + 1)) + min) / remainder;
-    return number;
-  } else {
-    return ('Некорректные значения. Пожалуйста, поменяйте.');
-  }
-};
-
-let RandomNumber = getRandomNumber(-1.5, 1.3, 4);
+const RandomNumber = getRandomNumber(10, 3, 2);
 alert(RandomNumber);
