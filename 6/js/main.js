98e10a21e1387ed3ed5e097b03f31283e20737df
Keks <keks@htmlacademy.ru>
2021-03-01 20:40:54 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,1 +1,5 @@
-import './popap.js';
+import { createTemporaryData } from './create-temporary-data.js';
+import { render } from './popup.js';
+
+const similarAds = createTemporaryData(1);
+render(similarAds[0]);
